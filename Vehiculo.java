bd3c0cf2a6cb2a97710d9b32538abc4081da8b8d
AdrianG-17 <116446014+AdrianG-17@users.noreply.github.com>
2022-10-22 07:59:50 -0700

    Update Vehiculo.java

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,3 +1,8 @@
-public class Vehiculo {
-    
+public class Vehiculo
+{
+    public static void main(String [] args){
+        final int ARRAYSIZE = 10;
+        int[] Vehiculo;
+        Vehiculo = new int[ARRAYSIZE];
+    }
 }
