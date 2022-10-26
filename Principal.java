2fb90c06bc6e18f8214d892567c10c1151b37958
NicolasTovar <nicolastovar9@gmail.com>
2022-10-25 20:47:55 -0500

    Avancefinal

--- Principal.java
+++ Principal.java
@@ -6,26 +6,28 @@ public class Principal{
     public static void mostrarmenu(){
         try (Scanner scan = new Scanner(System.in)) {
             while(true){
-                System.out.println("Si ingresa 0: Finaliza el Programa");
-                System.out.println("Si ingresa 1: Se piden parametros para alamcenar un vehiculo");
+                System.out.println("Si ingresa 0: Finalizar el Programa");
+                System.out.println("Si ingresa 1: Almacenar un vehiculo");
                 System.out.println("Si ingresa 2: Informacion de todos los vehiculos");
-                System.out.println("Si ingresa 3: Cantidad de Vehiculos");
+                System.out.println("Si ingresa 3: Cantidad de vehiculos");
                 System.out.println("Si ingresa 4: Informacion de todos los vehiculos verdes");
                 System.out.println("Si ingresa 5: Todos los vehiculos entre el 2000-2021");
-                System.out.println("Si ingresa 6: Se crea un Sensor con 2 parametros");
+                System.out.println("Si ingresa 6: Almacenar un sensor");
                 System.out.println("Si ingresa 7: Informacion de todos los sensores almacenados");
-                System.out.println("Si ingresa 8: Cantidad de sensores hasta el momento");
-                System.out.println("Si ingresa 9: Informacion de todos los sensores de tipo Temperatura");
-                System.out.println("Si ingresa 666: Todos los sensores de tipo Temperatura ordenados por valor");
+                System.out.println("Si ingresa 8: Cantidad de sensores");
+                System.out.println("Si ingresa 9: Informacion de todos los sensores de tipo temperatura");
+                System.out.println("Si ingresa 666: Todos los sensores de tipo temperatura ordenados por valor");
                 System.out.println("Ingresa un numero: ");
                 int numero = scan.nextInt();
 
                 if (numero==0){
+                    System.out.println("Programa Finalizado "); 
                     break;
                 }
                 else if(numero==1){
                     int tamanov = Vehiculo.posAnadir;
                     if (tamanov<10){
+                        System.out.println("\n");
                         System.out.println("Modelo: ");
                         int modelo = scan.nextInt();
                         scan.nextLine();
@@ -44,48 +46,58 @@ public class Principal{
                         else{
                             Vehiculo.vehiculos[Vehiculo.posAnadir]= new Vehiculo(modelo,marca,valorComercial,color);
                         }
-                        
+                        System.out.println("\n");
                         
                         Vehiculo.posAnadir++;
                         
                     }
                     else{
+                        System.out.println("\n");
                         System.out.println("Error base de datos llena");
+                        System.out.println("\n");
                     }
                 }
                 else if(numero==2){
-                    //Imprimir toda la informacion de los objetos vehiculos                
+                    //Imprimir toda la informacion de los objetos vehiculos   
+                    System.out.println("\n");             
                     for(int i=0; i<Vehiculo.posAnadir;i++){
                         System.out.println(Vehiculo.vehiculos[i].toString());                    
                     }                
+                    System.out.println("\n");
                 }
                 else if(numero==3){
                     //Imprimir la cantidad de vehiculos almacenados
-
+                    System.out.println("\n");
                     System.out.println("Cantidad de vehiculos almacenados: "+ Vehiculo.cantidadVehiculos());
+                    System.out.println("\n");
                 }
                 else if(numero==4){
                     //Imprimir info por pantalla de los verdes
+                    System.out.println("\n");
                     for(int i=0; i<Vehiculo.posAnadir;i++){
                         if(Vehiculo.vehiculos[i].getColor().equals("verde")){
                             System.out.println(Vehiculo.vehiculos[i].toString());
                         }
                     }
+                    System.out.println("\n");
                 }
                 else if(numero==5){
                     //imprimir info de los vehículos que tengan modelo entre 2000 y 2021 
+                    System.out.println("\n");
                     for(int i=0; i<Vehiculo.posAnadir;i++){
                         if(Vehiculo.vehiculos[i].getModelo()>2000 && Vehiculo.vehiculos[i].getModelo()<2021){
                             System.out.println(Vehiculo.vehiculos[i].toString());
                         }
                     }
+                    System.out.println("\n");
                 }
 
                 else if(numero==6){
                     int tamanos = Sensor.posAnadir;
 
                     if (tamanos<8){
-    
+                        
+                        System.out.println("\n");
                         System.out.println("Tipo: ");
                         scan.nextLine();
                         String tipo = scan.nextLine();
@@ -98,19 +110,26 @@ public class Principal{
 
                         Sensor.sensores[Sensor.posAnadir]= new Sensor(tipo, valor);
                         Sensor.posAnadir++;
+                        System.out.println("\n");
                     }
                     else{
+                        System.out.println("\n");
                         System.out.println("Error base de datos llena");
+                        System.out.println("\n");
                     }
                 }
                 else if(numero==7){
                     //Imprimir toda la informacion de los objetos sensores
+                    System.out.println("\n");
                     System.out.println(Sensor.toStringSensores());
                     System.out.println("\n");
                 }
                 else if(numero==8){
-                    //Imprimir la cantidad de sensores almacenados
+                     //Imprimir la cantidad de sensores almacenados
+                    System.out.println("\n");                   
                     System.out.println("cantidad de sensores almacenados: "+ Sensor.cantidadSensores());
+                    System.out.println("\n");
+
                 }
                 else if(numero==9){
                     //imprimir info por pantalla de los tipo temperatura
